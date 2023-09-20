a27f6c8484a4396101e3159fea35a8b25bfd3f6a
fzcnbch0 <119489401+fzcnbch0@users.noreply.github.com>
2023-09-20 08:46:28 +0200

    Update dowolny.js

--- 18.09/dowolny.js
+++ 18.09/dowolny.js
@@ -1,12 +1,34 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+const targetNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
-var myEventHandler = function () {
-  console.log('I hear a scream!');
+function guessNumber() {
+  rl.question('Guess the number (1-100): ', (guess) => {
+    const userGuess = parseInt(guess);
+
+    if (isNaN(userGuess)) {
+      console.log('Please enter a valid number.');
+    } else {
+      attempts++;
+
+      if (userGuess === targetNumber) {
+        console.log(`Congratulations! You guessed it in ${attempts} attempts.`);
+        rl.close();
+      } else if (userGuess < targetNumber) {
+        console.log('Too low! Try again.');
+        guessNumber();
+      } else {
+        console.log('Too high! Try again.');
+        guessNumber();
+      }
+    }
+  });
 }
 
-eventEmitter.on('scream', myEventHandler);
-
-
-eventEmitter.emit('scream');
+console.log('Welcome to the Number Guessing Game!');
+guessNumber();
